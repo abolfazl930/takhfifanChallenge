8a8f9cba821b8f9d5cf87865ba681e42eb32a6cb
abolfazl ahmadi <a.abolfazl930@gmail.com>
2019-01-06 15:10:27 +0330

    finished fill scale

--- js/index.js
+++ js/index.js
@@ -1,22 +1,36 @@
 $(function(){
 
-    function addClassMolileImage(){
-        $("#main__layer-one__animate-image-nexus").addClass("main__layer-one__animate-image-iphone--clicked");
-        $("#main__layer-one__animate-image-iphone").addClass("main__layer-one__animate-image-nexus--clicked");
-    }
-
-    function addClassSocialMeidia(){
+function molileImageShow(){
+    $("#main__layer-one__animate-image-nexus").addClass("main__layer-one__animate-image-iphone--clicked");
+    $("#main__layer-one__animate-image-iphone").addClass("main__layer-one__animate-image-nexus--clicked");
+}
+function molileImageHidden(){
+    $("#main__layer-one__animate-image-nexus").hide();
+    $("#main__layer-one__animate-image-iphone").hide();
+}
+    function socialMeidiaShow(){
         $("#main__layer-one__social-media").addClass("main__layer-one__social-media--clicked")
     }
 
-    $("#main__event").on("click",function(e){
-        e.stopPropagation();
-        addClassMolileImage();
-        addClassSocialMeidia();
+    function downloadBoxHidden(){
+        $("#main__layer-one__download-box").hide();
+    }
+
+    function addClassMainCircle(){
+        $("#main__circle").addClass("main__circle--clicked");
+    }
+
+
+    $("#main__event").on("click",function(){
+        molileImageShow();
+        socialMeidiaShow();
     });
 
-    $("button").on("click",function(){
-        $("#main__layer-one__download-box").css("display","none")
-        $("#main__circle").addClass("main__circle--clicked");
-    })
+    $("button").on("click",function(e){
+        e.stopPropagation();
+        downloadBoxHidden();
+        molileImageHidden();
+        addClassMainCircle();
+    });
+
 });
