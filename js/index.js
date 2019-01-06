951b95b30cff4acf18b82185004adcc13396b5b5
abolfazl ahmadi <a.abolfazl930@gmail.com>
2019-01-06 18:44:30 +0330

    dynamic fill page

--- js/index.js
+++ js/index.js
@@ -1,36 +1,42 @@
-$(function(){
-
-function molileImageShow(){
-    $("#main__layer-one__animate-image-nexus").addClass("main__layer-one__animate-image-iphone--clicked");
-    $("#main__layer-one__animate-image-iphone").addClass("main__layer-one__animate-image-nexus--clicked");
-}
-function molileImageHidden(){
+$(function() {
+  function molileImageShow() {
+    $("#main__layer-one__animate-image-nexus").addClass(
+      "main__layer-one__animate-image-iphone--clicked"
+    );
+    $("#main__layer-one__animate-image-iphone").addClass(
+      "main__layer-one__animate-image-nexus--clicked"
+    );
+  }
+  function molileImageHidden() {
     $("#main__layer-one__animate-image-nexus").hide();
     $("#main__layer-one__animate-image-iphone").hide();
-}
-    function socialMeidiaShow(){
-        $("#main__layer-one__social-media").addClass("main__layer-one__social-media--clicked")
-    }
+  }
+  function socialMeidiaShow() {
+    $("#main__layer-one__social-media").addClass(
+      "main__layer-one__social-media--clicked"
+    );
+  }
 
-    function downloadBoxHidden(){
-        $("#main__layer-one__download-box").hide();
-    }
+  function downloadBoxHidden() {
+    $("#main__layer-one__download-box").hide();
+  }
 
-    function addClassMainCircle(){
-        $("#main__circle").addClass("main__circle--clicked");
-    }
+  function addClassMainCircle() {
+    $("#main__circle").addClass("main__circle--clicked");
+  }
 
+  var width = Math.max(window.innerWidth, window.innerHeight);
+  $("#main__circle").css({ width: width, height: width });
 
-    $("#main__event").on("click",function(){
-        molileImageShow();
-        socialMeidiaShow();
-    });
+  $("#main__event").on("click", function() {
+    molileImageShow();
+    socialMeidiaShow();
+  });
 
-    $("button").on("click",function(e){
-        e.stopPropagation();
-        downloadBoxHidden();
-        molileImageHidden();
-        addClassMainCircle();
-    });
-
+  $("button").on("click", function(e) {
+    e.stopPropagation();
+    downloadBoxHidden();
+    molileImageHidden();
+    addClassMainCircle();
+  });
 });
