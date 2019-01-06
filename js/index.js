da985369d304fedc9b71dd17a77f0265dabda0e7
abolfazl ahmadi <a.abolfazl930@gmail.com>
2019-01-06 13:43:58 +0330

    test branch

--- js/index.js
+++ js/index.js
@@ -9,8 +9,14 @@ $(function(){
         $("#main__layer-one__social-media").addClass("main__layer-one__social-media--clicked")
     }
 
-    $("#main__layer-one").on("click",function(){
+    $("#main__event").on("click",function(e){
+        e.stopPropagation();
         addClassMolileImage();
         addClassSocialMeidia();
+    });
+
+    $("button").on("click",function(){
+        $("#main__layer-one__download-box").css("display","none")
+        $("#main__circle").addClass("main__circle--clicked");
     })
 });
