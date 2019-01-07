7734d54632ca6cee802bd0705b53eb19113c6b47
Your Name <you@example.com>
2019-01-07 04:56:03 +0330

    finished

--- js/index.js
+++ js/index.js
@@ -2,67 +2,72 @@ $(function () {
 
   var $$width = Math.max(window.innerWidth, window.innerHeight);
 
+  // functions START
   function molileImageShow() {
-    $("#main__layer-one__animate-image-nexus").addClass(
-      "main__layer-one__animate-image-iphone--clicked"
+    $("#main__animate-image-nexus").addClass(
+      "main__animate-image-iphone--clicked"
     );
-    $("#main__layer-one__animate-image-iphone").addClass(
-      "main__layer-one__animate-image-nexus--clicked"
+    $("#main__animate-image-iphone").addClass(
+      "main__animate-image-nexus--clicked"
     );
   }
 
   function molileImageHidden() {
-    $("#main__layer-one__animate-image-nexus").hide();
-    $("#main__layer-one__animate-image-iphone").hide();
+    $("#main__animate-image-nexus").hide();
+    $("#main__animate-image-iphone").hide();
   }
 
   function socialMeidiaShow() {
-    $("#main__layer-one__social-media").addClass(
-      "main__layer-one__social-media--clicked"
+    $("#main__social-media").addClass(
+      "main__social-media--clicked"
     );
   }
 
   function socialMeidiaHidden() {
-    $("#main__layer-one__social-media").addClass(
-      "main__layer-one__social-media--hidden"
+    $("#main__social-media").addClass(
+      "main__social-media--hidden"
     );
   }
 
   function downloadBoxHidden() {
-    $("#main__layer-one__download-box").hide();
+    $("#main__download-box").hide();
   }
 
   function addClassMainCircle() {
     $("#main__circle").addClass("main__circle--clicked");
   }
 
+  function searchImageShow() {
+    $(".main__search-image").addClass("main__search-image--clicked")
+  }
+
+  function categoryImageShow() {
+    $(".main__category-image").addClass("main__category-image--clicked")
+  }
+
+  function logoShow() {
+    $(".logo").addClass("logo-show");
+  }
+
+  function titleShow() {
+    $(".title").addClass("title-show");
+  }
+
+  function buttonWrapperShow() {
+    $(".button-wrapper").addClass("button-wrapper--clicked");
+  }
+  // functions END
+
+  //event START
+  $(".button-wrapper__button").on("click", function () {
+    $(this).siblings(".button-wrapper__dropdown-wrapper__dropdown-content").slideToggle();
+  });
 
   $("#main__circle").css({ width: $$width, height: $$width });
 
   $("#main__event").on("click", function () {
     molileImageShow();
     socialMeidiaShow();
-  });
-
-
-  function searchImageShow() {
-    $(".main__layer-two__search-image").addClass("main__layer-two__search-image--clicked")
-  }
-  function categoryImageShow() {
-    $(".main__layer-two__category-image").addClass("main__layer-two__category-image--clicked")
-  }
-  function logoShow() {
-    $(".logo").addClass("logo-show");
-  }
-  function titleShow() {
-    $(".title").addClass("title-show");
-  }
-  function buttonWrapperShow() {
-    $(".button-wrapper").addClass("button-wrapper--clicked");
-  }
-
-  $(".button-wrapper__button").on("click", function () {
-    $(this).siblings(".button-wrapper__dropdown-wrapper__dropdown-content").slideToggle();
   });
 
   $("#app-download-btn").on("click", function (e) {
@@ -77,7 +82,7 @@ $(function () {
     titleShow();
     buttonWrapperShow();
   });
-
+  //event END
 
 
 });
