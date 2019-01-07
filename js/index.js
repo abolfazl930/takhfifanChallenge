0cfb3f3013bc5a5f41e8d5f15f4a1f3c4af8e5fb
Your Name <you@example.com>
2019-01-07 04:37:22 +0330

    finished

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-$(function() {
+$(function () {
+
+  var $$width = Math.max(window.innerWidth, window.innerHeight);
+
   function molileImageShow() {
     $("#main__layer-one__animate-image-nexus").addClass(
       "main__layer-one__animate-image-iphone--clicked"
@@ -7,13 +10,21 @@ $(function() {
       "main__layer-one__animate-image-nexus--clicked"
     );
   }
+
   function molileImageHidden() {
     $("#main__layer-one__animate-image-nexus").hide();
     $("#main__layer-one__animate-image-iphone").hide();
   }
+
   function socialMeidiaShow() {
     $("#main__layer-one__social-media").addClass(
       "main__layer-one__social-media--clicked"
+    );
+  }
+
+  function socialMeidiaHidden() {
+    $("#main__layer-one__social-media").addClass(
+      "main__layer-one__social-media--hidden"
     );
   }
 
@@ -25,18 +36,48 @@ $(function() {
     $("#main__circle").addClass("main__circle--clicked");
   }
 
-  var width = Math.max(window.innerWidth, window.innerHeight);
-  $("#main__circle").css({ width: width, height: width });
 
-  $("#main__event").on("click", function() {
+  $("#main__circle").css({ width: $$width, height: $$width });
+
+  $("#main__event").on("click", function () {
     molileImageShow();
     socialMeidiaShow();
   });
 
-  $("button").on("click", function(e) {
+
+  function searchImageShow() {
+    $(".main__layer-two__search-image").addClass("main__layer-two__search-image--clicked")
+  }
+  function categoryImageShow() {
+    $(".main__layer-two__category-image").addClass("main__layer-two__category-image--clicked")
+  }
+  function logoShow() {
+    $(".logo").addClass("logo-show");
+  }
+  function titleShow() {
+    $(".title").addClass("title-show");
+  }
+  function buttonWrapperShow() {
+    $(".button-wrapper").addClass("button-wrapper--clicked");
+  }
+
+  $(".button-wrapper__button").on("click", function () {
+    $(this).siblings(".button-wrapper__dropdown-wrapper__dropdown-content").slideToggle();
+  });
+
+  $("#app-download-btn").on("click", function (e) {
     e.stopPropagation();
     downloadBoxHidden();
     molileImageHidden();
     addClassMainCircle();
+    socialMeidiaHidden();
+    searchImageShow();
+    categoryImageShow();
+    logoShow();
+    titleShow();
+    buttonWrapperShow();
   });
+
+
+
 });
